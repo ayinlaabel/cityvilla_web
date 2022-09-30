2d1312ee8729a8cf7df0ccd30f642ed0934c6139
Abel Ayinla <kraneabel@gmail.com>
2022-09-30 14:46:49 +0100

    Responsive add to the home page

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -6,22 +6,48 @@ import { Link } from "react-router-dom";
 function Navbar() {
   return (
     <div className="container">
-      <div className="logo">
-        <img src={logo} alt="City Villa Logo" />
+      <div className="desktop__menu">
+        <div className="logo">
+          <img src={logo} alt="City Villa Logo" />
+        </div>
+        <div className="menu">
+          <Link to="/" className="menu__item active">
+            Home
+          </Link>
+          <Link to="/about-us" className="menu__item">
+            About us
+          </Link>
+          <Link to="/" className="menu__item">
+            Projects
+          </Link>
+          <Link to="/" className="menu__item">
+            Contact us
+          </Link>
+        </div>
       </div>
-      <div className="menu">
-        <Link to="/" className="menu__item active">
-          Home
-        </Link>
-        <Link to="/about-us" className="menu__item">
-          About us
-        </Link>
-        <Link to="/" className="menu__item">
-          Projects
-        </Link>
-        <Link to="/" className="menu__item">
-          Contact us
-        </Link>
+      <div className="mobile__menu">
+        <div className="logo">
+          <img src={logo} alt="City Villa Logo" />
+        </div>
+        <div className="menu__btn">
+          <div className="btn__line"></div>
+          <div className="btn__line"></div>
+          <div className="btn__line"></div>
+        </div>
+        <div className="menu">
+          <Link to="/" className="menu__item active">
+            Home
+          </Link>
+          <Link to="/about-us" className="menu__item">
+            About us
+          </Link>
+          <Link to="/" className="menu__item">
+            Projects
+          </Link>
+          <Link to="/" className="menu__item">
+            Contact us
+          </Link>
+        </div>
       </div>
     </div>
   );
