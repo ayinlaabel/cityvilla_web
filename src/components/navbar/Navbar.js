86409bdd895d2307a024e59acfacf3abe0bd2c3d
Abel Ayinla <kraneabel@gmail.com>
2022-10-01 11:04:41 +0100

    Updated about page to be responsive

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,11 +1,29 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./navbar.scss";
 import logo from "../images/logo/cityvilla__logo.png";
 import { Link } from "react-router-dom";
 
 function Navbar() {
+  const ref = useRef(null);
+  let menuOpen = false;
+
+  const handleMenu = () => {
+    if (menuOpen === false) {
+      console.log(ref.current);
+      document.querySelector(".menu_m").classList.add("show");
+      document.querySelector(".mobile__menu").classList.add("show");
+
+      menuOpen = true;
+    } else {
+      console.log(ref.current);
+      document.querySelector(".menu_m").classList.remove("show");
+      document.querySelector(".mobile__menu").classList.remove("show");
+
+      menuOpen = false;
+    }
+  };
   return (
-    <div className="container">
+    <div  className="container">
       <div className="desktop__menu">
         <div className="logo">
           <img src={logo} alt="City Villa Logo" />
@@ -25,16 +43,16 @@ function Navbar() {
           </Link>
         </div>
       </div>
-      <div className="mobile__menu">
+      <div ref={ref} className="mobile__menu">
         <div className="logo">
           <img src={logo} alt="City Villa Logo" />
         </div>
-        <div className="menu__btn">
+        <div className="menu__btn" onClick={handleMenu}>
           <div className="btn__line"></div>
           <div className="btn__line"></div>
           <div className="btn__line"></div>
         </div>
-        <div className="menu">
+        <div ref={ref} className="menu_m">
           <Link to="/" className="menu__item active">
             Home
           </Link>
